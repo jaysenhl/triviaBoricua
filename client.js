65a57b237d29a59bbc961d28a405f78ec921e304
Jaysen <jaysenhdezlopez@gmail.com>
2024-03-28 11:55:52 -0400

    fixed url name

--- client.js
+++ client.js
@@ -17,7 +17,7 @@ let api_url;
 if (window.location.hostname === 'localhost') {
     api_url = 'http://localhost:8888/.netlify/functions/trivia';
 } else {
-    api_url = 'https://tudominio.netlify.app/.netlify/functions/trivia';
+    api_url = 'https://boricuatriviatest.netlify.app/.netlify/functions/trivia';
 }
 
 async function getQuestion() {
