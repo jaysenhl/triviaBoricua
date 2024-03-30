093f264650b9aeb815455ba9c78836926e4c9ea1
Jaysen <jaysenhdezlopez@gmail.com>
2024-03-30 08:27:57 -0400

    grouped variables and functions to another order

--- client.js
+++ client.js
@@ -6,16 +6,16 @@
 
 const questionAmount = document.getElementById('questionAmountInput')
 const restartBtn = document.getElementById('restartBtn')
-restartBtn.style.display = 'none'
 const addPlayerBtn = document.getElementById('addPlayerBtn')
-const createPlayerContainer = document.getElementById('createPlayerContainer')
-createPlayerContainer.style.display = 'none'
 const startGameBtn = document.getElementById('startGameBtn')
 const questionCard = document.getElementById('questionCard')
-questionCard.style.display = 'none'
+const createPlayerContainer = document.getElementById('createPlayerContainer')
 const playerNameInput = document.getElementById('playerNameInput')
 const multiQuestionAnswerContainer = document.getElementById('multiQuestionAnswerContainer')
 const booleanAnswerContainer = document.getElementById('booleanAnswerContainer')
+restartBtn.style.display = 'none'
+createPlayerContainer.style.display = 'none'
+questionCard.style.display = 'none'
 
 let playerPoints = 0
 let questions = [];
@@ -30,19 +30,6 @@ if (window.location.hostname === 'localhost') {
         api_url = 'https://boricuatriviatest.netlify.app/.netlify/functions/trivia';
     }
 
-addPlayerBtn.addEventListener('click', ()=>{
-    createPlayerContainer.style.display = 'block'
-    addPlayerBtn.style.display = 'none'
-    restartBtn.style.display = 'block'
-})
-
-restartBtn.addEventListener('click',()=>location.reload())
-
-function getPlayerName(){
-    let playerName = playerNameInput.value
-    return playerName
-}
-
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -50,6 +37,14 @@ function shuffleArray(array) {
     }
     return array;
 }
+
+addPlayerBtn.addEventListener('click', ()=>{
+    createPlayerContainer.style.display = 'block'
+    addPlayerBtn.style.display = 'none'
+    restartBtn.style.display = 'block'
+})
+
+restartBtn.addEventListener('click',()=>location.reload())
 
 async function getQuestion() {
     try {
@@ -62,6 +57,47 @@ async function getQuestion() {
     } catch (error) {
         console.error('Error fetching questions:', error);
         return []; // Devuelve un array vacío en caso de error para manejo seguro posterior
+    }
+}
+
+async function startGame() {
+    const totalQuestions = parseInt(questionAmount.value, 10); // Obtiene el valor como un número
+    const maxQuestions = 46; // Límite máximo de preguntas
+
+    // Verifica si el usuario ingresó más preguntas de las permitidas
+    if (totalQuestions > maxQuestions) {
+        // Muestra una alerta indicando el límite
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: `El límite máximo de preguntas es de ${maxQuestions}. Por favor, ingresa un número menor.`,
+            confirmButtonText: 'Intentar de nuevo'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Reinicia el estado del juego o la entrada para que el usuario pueda intentar nuevamente
+                createPlayerContainer.style.display = 'block';
+                questionCard.style.display = 'none';
+                questionAmount.value = ''; // Limpia el input
+            }
+        });
+        return; // Detiene la ejecución de la función para que el usuario pueda corregir la entrada
+    }
+
+    createPlayerContainer.style.display = 'none'; // Oculta el contenedor de añadir jugador
+    questionCard.style.display = 'block'; // Muestra el contenedor de la pregunta
+
+    let data = await getQuestion();
+    if (data && data.length > 0) {
+        questions = shuffleArray(data); // Mezclar las preguntas
+
+        if (totalQuestions > 0) {
+            questions = questions.slice(0, totalQuestions);
+        }
+
+        currentIndex = 0;
+        displayQuestion();
+    } else {
+        console.log("No se pudieron cargar las preguntas");
     }
 }
 
@@ -105,48 +141,67 @@ function displayQuestion() {
     }
 }
 
-startGameBtn.addEventListener('click', startGame);
-
-async function startGame() {
-    const totalQuestions = parseInt(questionAmount.value, 10); // Obtiene el valor como un número
-    const maxQuestions = 46; // Límite máximo de preguntas
-
-    // Verifica si el usuario ingresó más preguntas de las permitidas
-    if (totalQuestions > maxQuestions) {
-        // Muestra una alerta indicando el límite
+function checkAnswer(selectedAnswer) {
+    const currentQuestion = questions[currentIndex];
+    if (selectedAnswer === currentQuestion.correct_answer) {
+        playerPoints += 10;
+        correctAnswersCount++; // Incrementa el contador de respuestas correctas
         Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: `El límite máximo de preguntas es de ${maxQuestions}. Por favor, ingresa un número menor.`,
-            confirmButtonText: 'Intentar de nuevo'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                // Reinicia el estado del juego o la entrada para que el usuario pueda intentar nuevamente
-                createPlayerContainer.style.display = 'block';
-                questionCard.style.display = 'none';
-                questionAmount.value = ''; // Limpia el input
-            }
-        });
-        return; // Detiene la ejecución de la función para que el usuario pueda corregir la entrada
+            icon: "success",
+            title: "Correcto!",
+            text: `${currentQuestion.correct_answer}`,
+            footer: `<h3>+10 puntos!</h3>`
+          });
+    } else {
+        playerPoints -= 10;
+        incorrectAnswersCount++; // Incrementa el contador de respuestas incorrectas
+        Swal.fire({
+            icon: "error",
+            title: "Incorrecto!",
+            text: `La respuesta correcta es: ${currentQuestion.correct_answer}`,
+            footer: `<h3>-10 puntos!</h3>`
+          });
     }
 
-    createPlayerContainer.style.display = 'none'; // Oculta el contenedor de añadir jugador
-    questionCard.style.display = 'block'; // Muestra el contenedor de la pregunta
-
-    let data = await getQuestion();
-    if (data && data.length > 0) {
-        questions = shuffleArray(data); // Mezclar las preguntas
-
-        if (totalQuestions > 0) {
-            questions = questions.slice(0, totalQuestions);
-        }
-
-        currentIndex = 0;
+    currentIndex++;
+    if (currentIndex < questions.length) {
         displayQuestion();
     } else {
-        console.log("No se pudieron cargar las preguntas");
+        showFinalResults();
     }
 }
+
+function showFinalResults() {
+    const totalQuestions = correctAnswersCount + incorrectAnswersCount;
+    const percentage = Math.round((correctAnswersCount / totalQuestions) * 100);
+
+    Swal.fire({
+        title: '¡Las preguntas se acabaron!',
+        html: `
+            <p>Total de preguntas: ${totalQuestions}</p>
+            <p>Correctas: ${correctAnswersCount}</p>
+            <p>Incorrectas: ${incorrectAnswersCount}</p>
+            <p>Eres un ${percentage}% Boricua</p>
+        `,
+        icon: 'info',
+        confirmButtonText: 'Reiniciar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.reload(); // Recarga la página y reinicia el juego
+
+            // TODO: En un futuro, puedes implementar la lógica para guardar los resultados en una base de datos aquí.
+            // Esto podría implicar enviar los resultados a un servidor mediante una solicitud HTTP
+            // y luego manejar esa solicitud en el servidor para almacenar los resultados en la base de datos.
+        }
+    });
+}
+
+function getPlayerName(){
+    let playerName = playerNameInput.value
+    return playerName
+}
+
+startGameBtn.addEventListener('click', startGame);
 
 function createPlayerInfoComponent(){
     let playerName = getPlayerName()
@@ -210,59 +265,4 @@ function createMultipleAnswerComponent(answer1,answer2,answer3,answer4){
     multipleAnswerComponent.appendChild(answersComponent)
     questionCard.appendChild(multipleAnswerComponent)
 
-}
-
-function checkAnswer(selectedAnswer) {
-    const currentQuestion = questions[currentIndex];
-    if (selectedAnswer === currentQuestion.correct_answer) {
-        playerPoints += 10;
-        correctAnswersCount++; // Incrementa el contador de respuestas correctas
-        Swal.fire({
-            icon: "success",
-            title: "Correcto!",
-            text: `${currentQuestion.correct_answer}`,
-            footer: `<h3>+10 puntos!</h3>`
-          });
-    } else {
-        playerPoints -= 10;
-        incorrectAnswersCount++; // Incrementa el contador de respuestas incorrectas
-        Swal.fire({
-            icon: "error",
-            title: "Incorrecto!",
-            text: `La respuesta correcta es: ${currentQuestion.correct_answer}`,
-            footer: `<h3>-10 puntos!</h3>`
-          });
-    }
-
-    currentIndex++;
-    if (currentIndex < questions.length) {
-        displayQuestion();
-    } else {
-        showFinalResults();
-    }
-}
-
-function showFinalResults() {
-    const totalQuestions = correctAnswersCount + incorrectAnswersCount;
-    const percentage = Math.round((correctAnswersCount / totalQuestions) * 100);
-
-    Swal.fire({
-        title: '¡Las preguntas se acabaron!',
-        html: `
-            <p>Total de preguntas: ${totalQuestions}</p>
-            <p>Correctas: ${correctAnswersCount}</p>
-            <p>Incorrectas: ${incorrectAnswersCount}</p>
-            <p>Eres un ${percentage}% Boricua</p>
-        `,
-        icon: 'info',
-        confirmButtonText: 'Reiniciar'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            window.location.reload(); // Recarga la página y reinicia el juego
-
-            // TODO: En un futuro, puedes implementar la lógica para guardar los resultados en una base de datos aquí.
-            // Esto podría implicar enviar los resultados a un servidor mediante una solicitud HTTP
-            // y luego manejar esa solicitud en el servidor para almacenar los resultados en la base de datos.
-        }
-    });
 }
